9a465e9aca640c0fd9541176efc8f5c27b7734cb
Jakobi Haskell <jakobi_haskell@brown.edu>
2021-12-30 23:53:16 -0500

    adds comment, positioning is slightly off when resizing

--- src/FeedPost.js
+++ src/FeedPost.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import astronight from './sample/posts/astronight.png';
-import profileImg from './sample/posts/fakeProfileCircle.png';
-import './FeedPost.css';
+import React from "react";
+import astronight from "./sample/posts/astronight.png";
+import profileImg from "./sample/posts/fakeProfileCircle.png";
+import "./FeedPost.css";
 
 function FeedPost() {
+  // This is just a sample image we get from the public folder
+  // Basically, we will later update this variable with the image that the post should display.
+  const postSrc = astronight;
 
-    // This is just a sample image we get from the public folder
-    // Basically, we will later update this variable with the image that the post should display.
-    const postSrc= astronight;
-
-    return (
+  return (
     <>
-        <div id="container">
-            <div id="containerRect">
-                <img id="postImage" src={postSrc} />
-                <div id="authorContainer">
-                    <img id="profileImg" src={profileImg} />
-                    <p id="authorName">Johnny R.</p>
-                </div>
-                <h1 id="artTitle">Astronight</h1>
-            </div>
+      <div id="container">
+        <div id="containerRect">
+          <img id="postImage" src={postSrc} />
+          <div id="authorContainer">
+            <img id="profileImg" src={profileImg} />
+            <p id="authorName">Johnny R.</p>
+          </div>
+          <h1 id="artTitle">Astronight</h1>
+          <div className="commentDiv">
+            <img src={profileImg} className="commentImg" />
+            <p className="commentText">Awesome artwork! Keep it up!</p>
+          </div>
         </div>
+      </div>
     </>
-    )
+  );
 }
 
-export default FeedPost
+export default FeedPost;
