9ca11cd864b8f06fb4f7720ba8417dc8cb9d6f8d
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-01-01 16:26:05 -0500

    factored out followbtn code

--- src/Profile.js
+++ src/Profile.js
@@ -3,6 +3,7 @@ import profileImg from './sample/posts/profile2.png'
 import check from './sample/posts/check.svg'
 import plus from './sample/posts/plus.svg'
 import './Profile.css'
+import FollowBtn from "./FollowBtn"
 
 function Profile() {
     const [isFollowing, setIsFollowing] = useState(true);
@@ -12,11 +13,8 @@ function Profile() {
             <img className="profileImg" src={profileImg}></img>
            <h2 className="profileName">Johnny Rose</h2>
            <p className="username">@johnnyrose</p>
-           <p className="bio">This is my bio. I am the king of NFTs.</p>
-           {!isFollowing && <button id="followBtn" onClick={() => setIsFollowing(true)}><img id="plus" src={plus} />
-           Follow</button>}
-           {isFollowing && <button id="followingBtn" onClick={() => setIsFollowing(false)}><img id="check" src={check} />
-           Following</button>}
+           <p className="bio">This is my bio. I am the king of NFTs.</p>   
+           <FollowBtn></FollowBtn>
         </div>
       );
 }
