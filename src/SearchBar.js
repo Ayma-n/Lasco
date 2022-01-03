1dab20c769635d2fafb1bfcd63c0477305d34351
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-01-03 01:09:22 -0500

    fixes edge case of vh not taking into account safari browser height size on mobile

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,9 +4,21 @@ import './SearchBar.css'
 function SearchBar() {
     const [searchTerm, setSearchTerm] = useState("");
 
-    window.onbeforeunload = function () {
-        window.scrollTo(0, 0);
-      }
+    // window.onbeforeunload = function () {
+    //     window.scrollTo(0, 0);
+    //   }
+
+       // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
 
     function handleFocus() {
         document.getElementById("placeholder").style.display = 'none';
