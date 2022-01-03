ce37df886b8a835b8b870d3413a7265b938e2e11
Ayma-n <aymanbt14@gmail.com>
2022-01-03 18:01:10 -0500

    Adds first version of Login page UI

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
+import "./Login.css";
+
 function Login() {
     return (<>
-        <h1>Test</h1>
-    </>  );
+        <div id="container">
+            <div id="loginRect">
+                <div id="inputSec">
+
+                </div>
+                <div id="welcomeText">Welcome <span id="backColor">Back.</span></div>
+                <form>
+                    <input id="username" type="text" placeholder="Username" />
+                    <input id="password" type="password" placeholder='Password' />
+                    <button id="loginButton">Login</button>
+                </form>
+            </div>
+            {/* <div id="imgDiv">
+            </div> */}
+        </div>
+    </>);
 }
 
 export default Login;
