2c1533f7b9e0f1e353d82c5494f5ab861742838d
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-01-03 00:50:15 -0500

    half-working mobile search bar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,10 +4,16 @@ import './SearchBar.css'
 function SearchBar() {
     const [searchTerm, setSearchTerm] = useState("");
 
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+      }
+
     function handleFocus() {
         document.getElementById("placeholder").style.display = 'none';
-        document.getElementById("searchBar").style.top = "83px";
-        document.getElementById("searchBar").style.width = "1075px";
+        if (window.innerWidth > 650) {
+            document.getElementById("searchBar").style.top = "83px";
+            document.getElementById("searchBar").style.width = "1075px";
+        }
 
     }
 
@@ -24,18 +30,20 @@ function SearchBar() {
         if (isAt) {
             /* TODO: use ternary operator */
             document.getElementById("searchText").style.color = "rgb(195,55,245)";
+            document.getElementById("searchText").style.caretColor = "rgb(195,55,245)";
         }
         else {
             document.getElementById("searchText").style.color = "black";
+            document.getElementById("searchText").style.caretColor = "gray";
         }
         
       }
 
     return (<>
         <div id="searchBar" type="search">
-            <img id="magGlass" src={magGlass} />
+            <img id="magGlass" role="presentation" src={magGlass} />
             <p id="placeholder"> Find <span className='placeholderGradient'>Artists...</span></p>
-            <input 
+            <input
             // whileFocus={{y: 20}}
             id="searchText" type="search" value={searchTerm} onChange={handleChangeSearch} onFocus={handleFocus} onFocusOut={handleFocusOut} />
         </div>
