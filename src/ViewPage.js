09e592cb1ae42887c0849096a0a3c8a545f46b91
Jakobi Haskell <jakobi_haskell@brown.edu>
2022-01-03 19:10:03 -0500

    adds incomplete view page

--- src/ViewPage.js
+++ src/ViewPage.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import close from './sample/posts/close.svg';
 import fire from './sample/posts/fire.svg';
 import profileImg from './sample/posts/profile2.png'
-import testImg from './sample/posts/Rectangle 3.png'
+import testImg from './sample/posts/viewArt.jpeg'
 import './ViewPage.css'
 function View() {
     return (<>
@@ -14,14 +14,19 @@ function View() {
             <img src={fire} alt="fireIcon" id="like" />
             <p id="description">Lucious greens and blues drawn in Procreate.</p>
             <div id="price">$740</div>
-            <button id="buy">Buy</button>
+            <button id="buyBtn">Buy</button>
             </div>
             <div className="right">
                 <div id="comments">
                     <div className="commentDiv">
-                        <img src={profileImg} alt="" className="profileImg" />
-                        <div className="name"></div>
-                        <p className="comment"></p>
+                        <img src={profileImg} alt="profile" className="profileImg" />
+                        <div className="name">Kathleen R.</div>
+                        <p className="comment">Awesome artwork! Keep it up!</p>
+                    </div>
+                    <div className="commentDiv">
+                        <img src={profileImg} alt="profile" className="profileImg" />
+                        <div className="name">Richard J.</div>
+                        <p className="comment">I’d love to put this up in virtual gallery! Are you selling?</p>
                     </div>
                 </div>
             </div>
