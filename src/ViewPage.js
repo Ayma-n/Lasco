f44174e06c8baee924ab04d50a00b2bf354c6474
Jakobi Haskell <jakobi_haksell@brown.edu>
2022-01-04 15:11:35 -0500

    adding dynamic resizing, in progress

--- src/ViewPage.js
+++ src/ViewPage.js
@@ -27,6 +27,7 @@ function ViewPage() {
         </div>
         <div className="right">
           <div id="commentSec">
+          <div className="comment">
             <div className="profileImgDiv">
               <img src={profileImg} alt="profile" className="profileImg" />
             </div>
@@ -40,55 +41,7 @@ function ViewPage() {
               repudiandae velit ad. Eos repellat ipsa enim assumenda ea.
               Perspiciatis exercitationem eligendi.
             </div>
-            <div className="profileImgDiv">
-              <img src={profileImg} alt="profile" className="profileImg" />
             </div>
-            <div className="commentText">
-              <span className="name">Kathleen R.</span>Awesome artwork! Keep it
-              up! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur vel maiores aut qui voluptas atque laboriosam ullam
-              debitis ad nisi fugiat sunt quia, dolores aliquam iure harum
-              numquam eaque, doloremque ducimus, quae cumque obcaecati! Ipsa
-              nulla nobis voluptatem. Voluptas, numquam blanditiis quod
-              repudiandae velit ad. Eos repellat ipsa enim assumenda ea.
-              Perspiciatis exercitationem eligendi.
-            </div>
-            <div className="profileImgDiv">
-              <img src={profileImg} alt="profile" className="profileImg" />
-            </div>
-            <div className="commentText">
-              <span className="name">Kathleen R.</span>Awesome artwork! Keep it
-              up! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur vel maiores aut qui voluptas atque laboriosam ullam
-              debitis ad nisi fugiat sunt quia, dolores aliquam iure harum
-              numquam eaque, doloremque ducimus, quae cumque obcaecati! Ipsa
-              nulla nobis voluptatem. Voluptas, numquam blanditiis quod
-              repudiandae velit ad. Eos repellat ipsa enim assumenda ea.
-              Perspiciatis exercitationem eligendi.
-            </div>
-            <div className="profileImgDiv">
-              <img src={profileImg} alt="profile" className="profileImg" />
-            </div>
-            <div className="commentText">
-              <span className="name">Kathleen R.</span>Awesome artwork! Keep it
-              up! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur vel maiores aut qui voluptas atque laboriosam ullam
-              debitis ad nisi fugiat sunt quia, dolores aliquam iure harum
-              numquam eaque, doloremque ducimus, quae cumque obcaecati! Ipsa
-              nulla nobis voluptatem. Voluptas, numquam blanditiis quod
-              repudiandae velit ad. Eos repellat ipsa enim assumenda ea.
-              Perspiciatis exercitationem eligendi.
-            </div>
-            {/* <div className="commentDiv">
-            <div className="profileImgDiv">
-              <img src={profileImg} alt="profile" className="profileImg" />
-              </div>
-              <p className="commentText">
-                <span className="name">Richard J.</span>I’d love to put this up
-                in virtual gallery! Are you selling? ewsdfjdffdsdfsfdsfsddfsdfsfds
-                sdfdsffdsdfsfdssfdsdsdsdsfdfdfsdsffdfdsfdfdfdfds
-              </p>
-            </div> */}
           </div>
         </div>
       </div>
