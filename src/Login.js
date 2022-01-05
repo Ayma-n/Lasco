037294f37a14b601aa53813128b72dd9aeab2170
Ayma-n <aymanbt14@gmail.com>
2022-01-04 19:29:04 -0500

    Finished switching to CSS casing for Login

--- src/Login.js
+++ src/Login.js
@@ -5,19 +5,19 @@ import demoLogo from "./sample/posts/demo-logo.png";
 function Login() {
     return (<>
         <div id="Login">
-            <div id="loginRect">
-                <div id="welcomeText">Welcome <span class="magenta-text">Back.</span></div>
+            <div id="left-rect">
+                <div id="welcome-text">Welcome <span class="magenta-text">Back.</span></div>
                 <form>
                     <input id="username" type="text" placeholder="Username" />
                     <input id="password" type="password" placeholder='Password' />
-                    <button id="loginButton">Login</button>
+                    <button id="sign-button">Login</button>
                 </form>
-                <div id="logoDiv">
+                <div id="logo-div">
                     <img src={demoLogo}></img>
                     <div>Lasco</div>
                 </div>
             </div>
-            <div id="imgDiv"></div>
+            <div id="right-rect"></div>
         </div>
     </>);
 }
