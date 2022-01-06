bf1f04a778ff9626f5d218fee8fb01b129bd148e
Ayma-n <aymanbt14@gmail.com>
2022-01-06 18:37:47 -0500

    Right movement improved, overflow of left movement blocked

--- src/Landing.js
+++ src/Landing.js
@@ -8,28 +8,32 @@ import demoLogo from "./sample/posts/demo-logo.png";
 function Landing() {
     
     var belt;
+    var beltDiv;
     var scrollInterval;
 
+    const defaultWidthOffset = 20;
+    const defaultBeltWidth = 2662;
+
     function moveBeltLeft() {
-        if (parseInt(belt.style.left) == - 1 * 3204 + 1070) {
-            // alert("Hi")
-            belt.style.left = `${- 3204 + 535}px`;
+        const widthOffset = (defaultWidthOffset/defaultBeltWidth)*belt.clientWidth;
+        if (parseInt(beltDiv.style.left) == -belt.clientWidth) {
+            beltDiv.style.left = `${- belt.clientWidth - 0.5*widthOffset}px`;
 
         }
-        belt.style.left = parseInt(belt.style.left) + 1 + "px";
+        beltDiv.style.left = parseInt(beltDiv.style.left) + 1 + "px";
     }
 
     function moveBeltRight() {
-        if (parseInt(belt.style.left) == - 2 * 3204 + 1070) {
-            // alert("Hi")
-            belt.style.left = `${- 3204 + 535}px`;
-
+        const widthOffset = (defaultWidthOffset/defaultBeltWidth)*belt.clientWidth;
+        if (parseInt(beltDiv.style.left) == - 2 * belt.clientWidth - widthOffset) {
+            beltDiv.style.left = `${- belt.clientWidth - 0.5*widthOffset}px`;
         }
-        belt.style.left = parseInt(belt.style.left) - 1 + "px";
+        beltDiv.style.left = parseInt(beltDiv.style.left) - 1 + "px";
     }
     
     useEffect(() => {
-        belt = document.querySelector('#belt-div');
+        beltDiv = document.querySelector('#belt-div');
+        belt = document.querySelector('#main-belt');
         repeatWhileMouseOver(document.querySelector('#scroll-left-div'), moveBeltLeft, 1);
         repeatWhileMouseOver(document.querySelector('#scroll-right-div'), moveBeltRight, 1);
         scrolling(moveBeltRight, 10);
@@ -50,15 +54,16 @@ function Landing() {
     function scrolling(action, milliseconds) {
             scrollInterval = setInterval(action, milliseconds);
     }
-    console.log(window.innerWidth)
-    console.log(`${-4804 + 0.5 * window.innerWidth}px`);
+    //console.log(window.innerWidth)
+    //console.log(`${-4804 + 0.5 * window.innerWidth}px`);
+    
     return (<>
         <div id="Landing">
             <div id="intro-text">So you're curious about <span className="text-gradient"> NFTs</span> ?</div>
             <div id="belt-div"  style={{left: `${-3950 + 0.5 * window.innerWidth}px` }}>
-                <img id="belt" src={demoLandingBelt}></img>
-                <img id="belt" src={demoLandingBelt}></img>
-                <img id="belt" src={demoLandingBelt}></img>
+                <img className="belt" src={demoLandingBelt}></img>
+                <img id="main-belt" className="belt" src={demoLandingBelt}></img>
+                <img className="belt" src={demoLandingBelt}></img>
             </div>
             <div id="scroll-left-div"></div>
             <div id="scroll-right-div"></div>
