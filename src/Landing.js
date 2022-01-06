a638f445e39e5190f923bc1616d3532041df7935
Ayma-n <aymanbt14@gmail.com>
2022-01-05 21:15:08 -0500

    Finalized version of conveyer belt for Landing page

--- src/Landing.js
+++ src/Landing.js
@@ -4,7 +4,6 @@ import demoLandingBelt from "./sample/posts/Demo-Landing-Belt.png"
 
 function Landing() {
 
-    const [alfanita, setAlfanita] = useState(-620);
     var belt;
 
 
@@ -37,7 +36,7 @@ function Landing() {
         <div id="Landing">
             <div id="intro-text">So you're curious about <span className="text-gradient"> NFTs</span> ?</div>
             <div id="belt-div">
-                <img id="belt" style={{left: `${alfanita}px`}} src={demoLandingBelt}></img>
+                <img id="belt" style={{left: '-620px'}} src={demoLandingBelt}></img>
             </div>
             <div id="scroll-left-div"></div>
             <div id="scroll-right-div"></div>
