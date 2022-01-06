84e053960db7f64e5056553e49ac69d9d8416fb6
Ayma-n <aymanbt14@gmail.com>
2022-01-06 14:18:49 -0500

    Adds Login & Signup buttons - Style not complete

--- src/Landing.js
+++ src/Landing.js
@@ -6,7 +6,6 @@ function Landing() {
 
     var belt;
 
-
     function moveBeltLeft() {
         belt.style.left = parseInt(belt.style.left) + 1 + "px";
     }
@@ -40,6 +39,14 @@ function Landing() {
             </div>
             <div id="scroll-left-div"></div>
             <div id="scroll-right-div"></div>
+            <div id="button-div">
+                <a href="/login">
+                    <button class="login-signup">Login</button>
+                </a>
+                <a href="/signup">
+                    <button class="login-signup">Signup</button>
+                </a>
+            </div>
         </div>
     </>)
 }
