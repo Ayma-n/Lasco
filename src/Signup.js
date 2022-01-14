8445a504d5cc510368654dbc70f0b57fbafb7fa2
Ayma-n <aymanbt14@gmail.com>
2022-01-14 14:14:42 -0500

    Adds authentication handling for Signup component

--- src/Signup.js
+++ src/Signup.js
@@ -3,16 +3,35 @@ import "./Signup.css"
 import demoLogo from "./sample/posts/demo-logo.png";
 import { Link } from "react-router-dom";
 
+// This line imports the custom hook we created in AuthContext which returns
+// the AuthContext with a useContext() hook applied on it.
+import { useAuth } from "./contexts/AuthContext";
+
 function Signup() {
+    // We create refs for the data we will need to gather from the form.
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    // We get the usable AuthContext with useAuth().
+    const authContext = useAuth();
+
+    // We get the signup method from the auth context.
+    const signup = authContext.signup;
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        signup(emailRef.current.value, passwordRef.current.value);
+    }
+
     return (<>
         <div id="Signup">
             <div id="left-rect">
                 <div id="explore-text">Explore Art. <span className="magenta-text">Together.</span></div>
-                <form>
+                <form onSubmit="handleSubmit()">
                     <input id="name" type="text" placeholder="Name" />
-                    <input id="email" type="text" placeholder="E-mail"/>
+                    <input id="email" type="text" placeholder="E-mail" ref={emailRef}/>
                     <input id="username" type="text" placeholder="Username" />
-                    <input id="password" type="password" placeholder='Password' />
+                    <input id="password" type="password" placeholder='Password' ref={passwordRef} />
                     <select id="use-for" placeholder='What will you use Lasco for?'>
                         <option value="">What will you use Lasco for?</option>
                         <option value="buyer">Looking to buy art.</option>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { auth } from "../firebase"
 
 const AuthContext = React.createContext();
@@ -12,10 +12,28 @@ export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState();
 
     function signup(email, password) {
-        auth.createUserWithEmailAndPassword(email, password);
+        return auth.createUserWithEmailAndPassword(email, password);
     }
 
+    useEffect(handleAuthStateChanged, [])
+
+    // As soon as the state of authentication (on login/logout), a variable "user" is given
+    // and the state of currentUser is updated to that user.
+    // When the auth state changes, the listener returns a method that makes it possible to 
+    // unsubscribe from the listener (i.e., deactivate it).
+    // This is why we return the "unsubscribe" method in this function.
+    function handleAuthStateChanged() {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+        })
+
+        return unsubscribe;
+    }
+    
+
     const value = {
+        currentUser,
+        signup
     }
 
     return (
