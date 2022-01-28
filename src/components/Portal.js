4a2ee20509dbbc503774093ecc131981d7be1d20
Jakobi Haskell <jakobi_haksell@brown.edu>
2022-01-28 00:44:13 -0500

    adds hook for user object in database context

--- src/components/Portal.js
+++ src/components/Portal.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PortalNav from './PortalNav';
 import '../css/Portal.css';
 
 import { useAuth } from "../contexts/AuthContext";
+import { useDb } from "../contexts/DatabaseContext";
 import { useNavigate } from "react-router-dom";
 
 function Portal(props) {
-
+    const { userInfo} = useDb();
     // We import currentUser & signout from useAuth.
     const [error, setError] = useState('')
     const { currentUser, signout } = useAuth(); 
@@ -28,6 +29,7 @@ function Portal(props) {
             <PortalNav></PortalNav>
             {error && <div>{error}</div>}
             {currentUser && <div>Logged in as {currentUser.email}. <span onClick={handleLogout}>Sign Out</span></div>}
+            {userInfo && <div>Logged in as {userInfo.username}. <span onClick={handleLogout}>Sign Out</span></div>}
             {React.createElement(props.currentPage)}
         </div>
         )
--- src/contexts/DatabaseContext.js
+++ src/contexts/DatabaseContext.js
@@ -1,48 +1,73 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from "react";
 import { db } from "../components/firebase";
-import { collection, addDoc, getDoc, query, where, getDocs } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDoc,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { useAuth } from "./AuthContext";
 
 const DatabaseContext = React.createContext();
 
 export function useDb() {
-    return useContext(DatabaseContext)
+  return useContext(DatabaseContext);
 }
 
 export function DbProvider({ children }) {
-    
-    function createUser(userObject) {
-        const profilesRef = collection(db, '/profiles');
-        return addDoc(profilesRef, userObject);
-    }
+  const { currentUser } = useAuth();
 
-    async function getProfileData(uid) {
-        const profilesRef = collection(db, '/profiles');
-        const q = query(profilesRef, where("uid", "==", uid))
-        
-        const querySnapshot = await getDocs(q);
-        //console.log(queryResult);
-        //console.log(querySnapshot);
-        //console.log(uid);
-        //console.log(querySnapshot)
+  function createUser(userObject) {
+    const profilesRef = collection(db, "/profiles");
+    return addDoc(profilesRef, userObject);
+  }
 
-        const docs = [];
+  async function getProfileData(uid) {
+    const profilesRef = collection(db, "/profiles");
+    const q = query(profilesRef, where("uid", "==", uid));
 
-        querySnapshot.forEach((doc) => {
-            docs.push(doc.data());
-        })
+    const querySnapshot = await getDocs(q);
+    //console.log(queryResult);
+    //console.log(querySnapshot);
+    //console.log(uid);
+    //console.log(querySnapshot)
 
-        return docs[0];
-    }
+    const docs = [];
 
-    const value = {
-        createUser,
-        getProfileData
-    }
-    
+    querySnapshot.forEach((doc) => {
+      docs.push(doc.data());
+    });
 
-    return (
-        <DatabaseContext.Provider value={value}>
-            {children}
-        </DatabaseContext.Provider>
-    )
+    return docs[0];
+  }
+
+  const [userInfo, setUserInfo] = useState({ displayName: "", username: "" });
+
+  async function fetchUserData() {
+    //console.log(currentUser.uid);
+    if (!currentUser.uid) return;
+    const userInfoObj = await getProfileData(currentUser.uid);
+    //console.log(userInfoObj);
+    //console.log(userInfoObj);
+    //console.log(currentUser.uid);
+    setUserInfo(userInfoObj);
+    //console.log(userInfoObj);
+  }
+
+  //   useEffect(fetchUserData, []);
+  fetchUserData();
+
+  const value = {
+    createUser,
+    getProfileData,
+    userInfo,
+  };
+
+  return (
+    <DatabaseContext.Provider value={value}>
+      {children}
+    </DatabaseContext.Provider>
+  );
 }
